d9e7474db778c24926571532bf4a51db954c2890
berlianamartin <berlianadewintamartin@gmail.com>
2023-04-05 12:17:34 +0700

    {NF}Mahasiswa

---
+++ src/pertemuan1/Mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author USER
+ */
+public class Mahasiswa {
+    String nama;
+    
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nama = "berliana dewinta";
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.nama = "berliana martin";
+        
+        System.out.println(mhs1.nama);
+        System.out.println(mhs2.nama);
+        System.out.println("test");
+        
+    }
+}
